f3f1be7a10aeb7e89f15e5ea00a42b01fd001490
777rushi <777rushi@gmail.com>
2016-12-19 10:59:18 +1100

    add module

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'res partner extend',
+    'version': '1.0',
+    'author': 'Rushi Patel',
+    'depends': ['base'],
+    'data': [
+        'res_partner_view.xml',
+    ],
+    'application': True,
+    'auto_install': False,
+}
